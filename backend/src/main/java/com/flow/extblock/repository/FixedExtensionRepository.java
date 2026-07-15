b3ca39327c7c5495991b1182c3d5b3030018fb7b
wooykeong93 <dnrud368@gmail.com>
2026-07-16 00:07:17 +0900

    feat: 고정확장자 상태 변경시 정렬 순서 변경 오류 수정

--- backend/src/main/java/com/flow/extblock/repository/FixedExtensionRepository.java
+++ backend/src/main/java/com/flow/extblock/repository/FixedExtensionRepository.java
@@ -1,6 +1,9 @@
 package com.flow.extblock.repository;
 
 import com.flow.extblock.entity.FixedExtension;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -8,6 +11,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface FixedExtensionRepository extends JpaRepository<FixedExtension, Long> {
 
+    /**
+     * 전체 객체 조회시 아이디값으로 순번을 고정하여 조회한다 
+     * 객체 수정시 정렬이 변경되어 UX 에 영향을 미춰 변경 처리 함 
+     * @return 고정확장자 리스트(ID 값 기준 정렬)
+     */
+    List<FixedExtension> findAllByOrderIdAsc();
     /**
      * 주어진 이름을 가진 고정 확장자가 존재하는지 확인한다.
      * 커스텀 확장자 등록 시 교차 중복 체크에 사용된다.
